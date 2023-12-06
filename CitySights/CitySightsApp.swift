513d5e325ab5fdfcb2c7e1001838506f2507623c
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-06 11:12:18 +0300

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Omer Cagri Sayir on 6.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
