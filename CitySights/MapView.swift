c4120782b047dc7151e9f0c2a43c7428449172f6
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-09 05:39:36 +0300

    created mapview and added map in it

---
+++ CitySights/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  CitySights
+//
+//  Created by Omer Cagri Sayir on 9.12.2023.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    MapView()
+}
